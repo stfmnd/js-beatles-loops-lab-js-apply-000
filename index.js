28e3e7926ae7b4b58d9c8a1fc6595dac0ef68a26
Matthew <matthew.97.clark@gmail.com>
2019-10-09 19:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 // Task 2
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
 let newFacts = [];
 let i = 0;
 
