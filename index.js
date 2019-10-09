ad6f34fce940edefdd4dfc1c718b0b5d4235f078
Matthew <matthew.97.clark@gmail.com>
2019-10-09 19:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+// Task 1
+
+function theBeatlesPlay(musicians, instruments) {
+  let theBeatles = [];
+  
+  for(let i = 0; i < musicians.length; i++) {
+    const entry = musicians[i] + ' plays ' + instruments[i];
+    theBeatles.push(entry);
+  }
+  
+  return theBeatles;
+}
+
+// Task 2
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+let newFacts = [];
+let i = 0;
+
+function johnLennonFacts(fact) {
+  while (i < facts.length) {
+    newFacts.push(fact[i] += '!!!');
+    i++
+  }
+  return
+}
+
+johnLennonFacts(facts);
+
